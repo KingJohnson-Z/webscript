7cce5e2bbe95e9a37b5377cc8b8b7e40b22ca98c
KingJohnson <33320891+KingJohnson-Z@users.noreply.github.com>
2020-01-02 21:46:48 +0800

    Update qqmusic-lan-classify.js

--- qqmusic-lan-classify.js
+++ qqmusic-lan-classify.js
@@ -1,3 +1,23 @@
+/**
+* Only useful for qqmusic web!!!
+* @description This script helps you to classify your song by song language in your selected song list to a js object.
+* @return Language as key, and title well be the value.result will be printed in console.
+* @author KingJohnson
+* 
+* operations before run this script:
+* 1. first open and login QQmusic in browser。Go to your song list page. 
+* url for example:https://y.qq.com/portal/profile.html#sub=other&tab=create&
+* 2. open f12 and turn to 'network' tab before clicking any your song list. 
+* 3. clear all network load history and set filter to 'JS' in order to find our data easyily in next step.
+* 4. click your song list you want to classify and search this request in network load history:
+* fcg_musiclist_getinfo_cp.fcg?uin=xxxxxxx
+* 5. you will find 'SongList' in jsonCallback in preview tab.
+* 6. right click 'SongList' and select 'store as gobal variable'.Then tmp variable 'temp1' well be created in console.
+* 7. copy this script and just run.It well takes you some time to run out result, please wait......
+* 
+*/
+
+
 ((data)=>{
     let index=0;
     let length=data.length;
